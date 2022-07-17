ed53e69bf436d1eab952052f2be5399e0ac41974
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-07-17 22:12:14 +0300

    add logic

--- src/Example.tsx
+++ src/Example.tsx
@@ -3,20 +3,20 @@ import React, { Dispatch, SetStateAction } from 'react';
 export type CounterType = 0 | 1 | 2 | 3 | 4 | 5;
 
 export type CounterPropsType = {
-   setCounterValue: Dispatch<SetStateAction<number>>;
+   setCounterValue: Dispatch<SetStateAction<number>>
    counterValue: number;
    value: number;
 }
 
 export function Example(props: CounterPropsType) {
 
-
+   let maxValue: CounterType = 5;
 
    return (
       <div>
-         <div>{props.counterValue}</div>
-         <button onClick={() => { props.setCounterValue(props.value + 1) }}>Inc</button>
+         <span>{props.counterValue}</span>
+         <button onClick={() => { props.setCounterValue(props.counterValue < maxValue ? props.counterValue + 1 : maxValue) }}>inc</button>
          <button onClick={() => { props.setCounterValue(0) }}>res</button>
-      </div>
+      </div >
    );
 }
