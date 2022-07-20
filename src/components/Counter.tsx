68c1538be688ac84b1e489390bea734afdfa609c
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-07-20 23:39:25 +0300

    upd

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -8,6 +8,7 @@ export function Counter(props: CounterPropsType) {
    return (
       <div className='example'>
          <Display maxValue={props.maxValue} counterValue={props.counterValue} />
+         <Display maxValue={props.maxValue} counterValue={props.counterValue} />
          <Increment maxValue={props.maxValue} setCounterValue={props.setCounterValue} counterValue={props.counterValue} />
          <Reset maxValue={props.maxValue} setCounterValue={props.setCounterValue} counterValue={props.counterValue} />
       </div>
