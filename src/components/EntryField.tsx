669f9ebe75d467f2f8d9f5045c489f3b80ff0fff
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-07-23 21:41:11 +0300

    counter update

--- src/components/EntryField.tsx
+++ src/components/EntryField.tsx
@@ -7,12 +7,16 @@ type EntryFieldPropsType = {
 }
 
 export function EntryField(props: EntryFieldPropsType) {
+
+   let testFun = () => {
+   }
+
    return (
       <div className='entryfield'>
          Max value: <input type='number' className='entryfield-input' onChange={props.maxCounterValue} />
          <br />
          Start value:<input type='number' className='entryfield-input' onChange={props.startCounterValue} />
-         <button className='entryfield-button' onClick={props.entryFieldButton}>Set</button>
+         <button className='entryfield-button' onClick={testFun}>Set</button>
       </div>
    );
 }
