8a35846789bb601475125879f856b127561216b2
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-07-25 22:04:20 +0300

    created a const that was assigned a ternanor condition

--- src/components/Increment.tsx
+++ src/components/Increment.tsx
@@ -10,17 +10,11 @@ export function Increment(props: IncrementPropsType) {
    let changeCounterValue = () => {
       props.setCounterValue(props.counterValue < props.maxValue ? props.counterValue + 1 : props.maxValue)
    }
-   let styleDisplayCondition = () => {
-      props.counterValue === props.maxValue ? disButton : normButton
-   }
-   const disButton = {
-      pointerEvents: 'none',
-      cursor: 'default',
-      opacity: '0.3',
-   }
-   const normButton = {}
+   const disButton = { pointerEvents: 'none', cursor: 'default', opacity: '0.3', };
+   const normButton = {};
+   let styleDisplayCondition = props.counterValue === props.maxValue ? disButton : normButton;
 
    return (
-      <button style={styleDisplayCondition} className='button' onClick={changeCounterValue}></button>
+      <button style={styleDisplayCondition} className='button' onClick={changeCounterValue} />
    );
 }
