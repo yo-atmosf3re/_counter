4d9ca74fd298964b68b65f4989ae317e1da8aef6
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-07-26 22:55:04 +0300

    adding new functions, reworking UseState, reworking props, updating typing in the Counter component, reworking props in the Display, Increment, Reset components

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 export type DisplayType = {
    counterValue: number;
    maxValue: number;
+   startValue: number;
 }
 
 export function Display(props: DisplayType) {
    const screen = {
       color: props.counterValue === props.maxValue ? 'red' : 'white',
    }
+   const displayCondition =
+      props.startValue > props.maxValue ? 'startValue more than maxValue' : props.counterValue > props.maxValue ? 'counterValue more than maxValue' : props.startValue;
 
    return (
-      <span className='screen' style={screen}>{props.counterValue}</span>
+      <span className='screen' style={screen}>{displayCondition}</span>
    );
 }
